281e2b172e4796780ddbfa2fef117c0858fa45e3
Cobrettie <cobrettiegarner3@gmail.com>
2020-05-12 19:17:59 -0400

    add components directory + build LoginForm component template

---
+++ friends/src/components/LoginForm/LoginForm.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+export default function LoginForm() {
+  const [credentials, setCredentials] = useState({})
+
+  const handleChange = event => {
+    setCredentials({
+      ...credentials,
+      [event.target.name] : event.target.value
+    })
+  }
+
+  return (
+    <div>
+      <h2>Login Form</h2>
+
+      <div>
+        <form>
+          <input 
+            type='text'
+            name='username'
+            onChange={handleChange}
+            value={credentials.username}
+            placeholder='Username'
+          />
+          <input 
+            type='password'
+            name='password'
+            onChange={handleChange}
+            value={credentials.password}
+            placeholder='Password'
+          />
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    </div>
+  )
+}
