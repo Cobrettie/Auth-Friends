c2c080b4ca2c0640bddecc4bb55fb4255ceb6bc4
Cobrettie <cobrettiegarner3@gmail.com>
2020-05-13 19:34:59 -0400

    AddFriend POST request working adds a new friend

--- friends/src/components/Friends/AddFriend.js
+++ friends/src/components/Friends/AddFriend.js
@@ -4,6 +4,34 @@ import axios from 'axios';
 import NavMenu from '../NavMenu/NavMenu';
 
 class AddFriend extends React.Component {
+  state = {
+    friend: {
+      // id: 27,
+      name: 'friend one eleven',
+      age: 35,
+      email: 'your email here'
+    }
+  }
+
+  handleChange = event => {
+    this.setState({
+      friend: { 
+        ...this.state.friend,
+        [event.target.name] : event.target.value
+      }
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+
+    axios.post('http://localhost:5000/api/friends', this.state.friend)
+      // .then(response => {
+      //   console.log('AddFriend POST req res', response)
+      // })
+      // .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div>
@@ -11,26 +39,29 @@ class AddFriend extends React.Component {
         <h2>Add Friend Component</h2>
 
         <div>
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <input
               type='text'
               name='name'
+              onChange={this.handleChange}
               placeholder='name'
             />
             <input 
               type='email'
               name='email'
+              onChange={this.handleChange}
               placeholder='email'
             />
             <input 
-              type='text'
+              type='number'
               name='age'
+              onChange={this.handleChange}
               placeholder='age'
             />
             <button type='submit'>Add Friend</button>
           </form>
         </div>
-        
+
       </div>
     )
   }
