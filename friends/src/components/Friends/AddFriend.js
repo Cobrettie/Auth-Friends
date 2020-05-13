156040f9a7e860db7c1596fe9e6b288650828fac
Cobrettie <cobrettiegarner3@gmail.com>
2020-05-13 19:11:28 -0400

    edit AddFriend Component + build form boilerplate for AddFriend

--- friends/src/components/Friends/AddFriend.js
+++ friends/src/components/Friends/AddFriend.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import axios from 'axios';
 
-export default function AddFriend() {
-  return (
-    <div>
-      <h2>Add Friend Component</h2>
-    </div>
-  )
+import NavMenu from '../NavMenu/NavMenu';
+
+class AddFriend extends React.Component {
+  render() {
+    return (
+      <div>
+        <NavMenu />
+        <h2>Add Friend Component</h2>
+
+        <div>
+          <form>
+            <input
+              type='text'
+              name='name'
+              placeholder='name'
+            />
+            <input 
+              type='email'
+              name='email'
+              placeholder='email'
+            />
+            <input 
+              type='text'
+              name='age'
+              placeholder='age'
+            />
+          </form>
+        </div>
+
+      </div>
+    )
+  }
 }
